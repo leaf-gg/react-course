384eaa7a39ecede4f5ab9a91bda4fb5276b76a97
leaf-gg <jobs.lugui@gmail.com>
2023-10-24 22:17:54 -0300

    feat: exercise filtering and consuming data

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,57 @@
 import React from "react";
+
+// Organize os produtos como mostrado no vídeo
+// Mostre apenas produtos que forem mais caros que R$ 1500
+const produtos = [
+  {
+    id: 1,
+    nome: "Smartphone",
+    preco: "R$ 2000",
+    cores: ["#29d8d5", "#252a34", "#fc3766"],
+  },
+  {
+    id: 2,
+    nome: "Notebook",
+    preco: "R$ 3000",
+    cores: ["#ffd045", "#d4394b", "#f37c59"],
+  },
+  {
+    id: 3,
+    nome: "Tablet",
+    preco: "R$ 1500",
+    cores: ["#365069", "#47c1c8", "#f95786"],
+  },
+];
+
 const App = () => {
-  const products = ["prod1", "prod2", "prod3"];
-
-  const livros = [
-    { nome: 'A Game of Thrones', ano: 1996 },
-    { nome: 'A Clash of Kings', ano: 1998 },
-    { nome: 'A Storm of Swords', ano: 2000 },
-  ];
-
   return (
-    <ul>
-      {livros.filter(({ano}) => ano >= 1997).map(({nome, ano}) => ( 
-        <li key={nome}>{nome}, {ano}</li>
+    <section>
+      {produtos.map((item) => (
+        <div key={item.id} className="produto">
+          {Number(item.preco.replace("R$ ", "")) > 1500 && (
+            <>
+              <h1 key={item.nome}>{item.nome}</h1>
+              <p key={item.preco}>Preço: {item.preco}</p>
+
+              {item.cores.map((cor) => (
+                <div
+                  style={{
+                    backgroundColor: cor,
+                    color: "white",
+                    padding: "10px",
+                    margin: "10px 1rem",
+                    fontWeight: "500",
+                    textAlign: "center"
+                  }}
+                >
+                  {cor}
+                </div>
+              ))}
+            </>
+          )}
+        </div>
       ))}
-    </ul>
+    </section>
   );
 };
 
