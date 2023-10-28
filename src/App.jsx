19aa9b4d1949d1cd8733d2ecd92d2830d7cb53e2
leaf-gg <jobs.lugui@gmail.com>
2023-10-28 20:35:50 -0300

    feat: events

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,22 @@
 import React from "react";
 
-// Organize os produtos como mostrado no vídeo
-// Mostre apenas produtos que forem mais caros que R$ 1500
-const produtos = [
-  {
-    id: 1,
-    nome: "Smartphone",
-    preco: "R$ 2000",
-    cores: ["#29d8d5", "#252a34", "#fc3766"],
-  },
-  {
-    id: 2,
-    nome: "Notebook",
-    preco: "R$ 3000",
-    cores: ["#ffd045", "#d4394b", "#f37c59"],
-  },
-  {
-    id: 3,
-    nome: "Tablet",
-    preco: "R$ 1500",
-    cores: ["#365069", "#47c1c8", "#f95786"],
-  },
-];
 
 const App = () => {
-  const data = produtos.filter( ({preco}) => Number(preco.replace("R$ ", "")) > 1500);
-  
+  function handleClick(event){
+    console.log('event', event.target)
+  }
+  function handleScroll(event){
+    console.log(event)
+  }
+
+  window.addEventListener('scroll', handleScroll)
+
   return (
-    <section>
-      {data.map(({id, nome, preco, cores}) => 
-      <div key={id}>
-        <h1>{nome}</h1>
-         <p>Preço: {preco}</p>
-         <ul>
-            {cores.map(cor => <li style={{backgroundColor: cor, color: "white"}} key={cor}>{cor}</li>)}
-         </ul>
-      </div> 
-    )}
-    </section>
-  );
+    <div style={{height:'200vh'}}>
+      <button onClick={(event) => {alert(event.target.innerText)}}>Clique aqui</button>
+    </div>  
+  )
+
 };
 
 export default App;
